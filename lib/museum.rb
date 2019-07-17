52e92f5c33a7f6b5b98801a3d866bf471136f98f
Nancy Lee <srlee713@gmail.com>
2019-07-17 15:09:22 -0600

    Complete Iteration 3

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,11 @@
 class Museum
 
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(new_exhibit)
@@ -14,4 +15,24 @@ class Museum
   def recommend_exhibits(patron)
     @exhibits.select { |exhibit| patron.interests.include? exhibit.name }
   end
+
+  def admit(new_patron)
+    @patrons << new_patron
+  end
+
+  def patrons_by_exhibit_interest
+    results = Hash.new {|exhibit, patron| exhibit[patron]=[]}
+
+    @exhibits.each do |exhibit|
+      @patrons.each do |patron|
+        if patron.interests.include? exhibit.name
+          results[exhibit] << patron
+        else
+          results[exhibit]
+        end
+      end
+    end
+    results
+  end
+
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -3,6 +3,7 @@ require 'minitest/pride'
 require './lib/museum'
 require './lib/patron'
 require './lib/exhibit'
+require 'pry'
 
 class MuseumTest < Minitest::Test
 
@@ -35,6 +36,17 @@ class MuseumTest < Minitest::Test
     assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
   end
 
+  def test_it_has_no_patrons_initially
+    assert @dmns.patrons.empty?
+  end
+
+  def test_it_can_admit_patron
+    @dmns.admit(@bob)
+    @dmns.admit(@sally)
+
+    assert_equal [@bob, @sally], @dmns.patrons
+  end
+
   def test_it_can_recommend_exhibits
     @dmns.add_exhibit(@gems_and_minerals)
     @dmns.add_exhibit(@dead_sea_scrolls)
@@ -49,6 +61,27 @@ class MuseumTest < Minitest::Test
 
     assert_equal [@imax], @dmns.recommend_exhibits(@sally)
     refute @dmns.recommend_exhibits(@sally).include? @gems_and_minerals
+  end
+
+  def test_it_can_list_patrons_by_exhibit_interest
+    @bob.add_interest("Dead Sea Scrolls")
+    @bob.add_interest("Gems and Minerals")
+    @sally.add_interest("Dead Sea Scrolls")
+
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    @dmns.admit(@bob)
+    @dmns.admit(@sally)
+
+    expected = {
+      @gems_and_minerals => [@bob],
+      @dead_sea_scrolls => [@bob, @sally],
+      @imax => []
+    }
+
+    assert_equal expected, @dmns.patrons_by_exhibit_interest
   end
 
 end
